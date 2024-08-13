0b27623139a396453ca8b3e7bdf46132e833ed41
Elias <35974735+elias102@users.noreply.github.com>
2024-08-13 22:46:29 +0330

    fix test file

--- src/components/Table/__test__/Table.test.tsx
+++ src/components/Table/__test__/Table.test.tsx
@@ -5,7 +5,15 @@ import Table from "../Table";
 
 describe("Table component", () => {
   it("Table should render correctly", () => {
-    render(<Table />);
+    const headerCellList = ["ID", "FirstName", "LastName"];
+    const dataCellList = [
+      { id: 1, firstName: "elias", lastName: "sharafi" },
+      { id: 2, firstName: "sadra", lastName: "sharafi" },
+      { id: 3, firstName: "hermes", lastName: "sharafi" },
+    ];
+    render(
+      <Table headerCellList={headerCellList} dataCellList={dataCellList} />,
+    );
     const table = screen.getByRole("table");
     expect(table).toBeInTheDocument();
   });
